ae32c74ac88ce7ed5dceaef2b9d821582928d8a8
Erik Niebla <ep_niebla@hotmail.com>
2021-12-02 14:40:22 -0500

    Scope Roles/Permissions by user(and team when available)

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -2,6 +2,7 @@
 
 namespace Spatie\Permission\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Spatie\Permission\Contracts\Role as RoleContract;
@@ -83,6 +84,41 @@ class Role extends Model implements RoleContract
             app(PermissionRegistrar::class)->pivotRole,
             config('permission.column_names.model_morph_key')
         );
+    }
+
+    /**
+     * Scope the model query to certain user(and team) only.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  int|string|\Illuminate\Database\Eloquent\Model  $userId
+     * @param  null|int|string|\Illuminate\Database\Eloquent\Model  $teamId
+     */
+    public function scopeUser(Builder $query, $userId, $teamId = null): Builder
+    {
+        if ($userId instanceof Model) {
+            $userId = $userId->getKey();
+        }
+
+        if ($teamId instanceof Model) {
+            $teamId = $teamId->getKey();
+        }
+
+        if (app(PermissionRegistrar::class)->teams) {
+            $teamId = $teamId ?: getPermissionsTeamId();
+
+            $query->where(function ($query) use ($teamId) {
+                $query->whereNull(app(PermissionRegistrar::class)->teamsKey)
+                    ->orWhere(app(PermissionRegistrar::class)->teamsKey, $teamId);
+            });
+        }
+
+        return $query->whereHas('users', function ($query) use ($userId, $teamId) {
+            $query->where(config('permission.table_names.model_has_roles').'.'.config('permission.column_names.model_morph_key'), $userId);
+
+            if (app(PermissionRegistrar::class)->teams) {
+                $query->where(app(PermissionRegistrar::class)->teamsKey, $teamId);
+            }
+        });
     }
 
     /**
