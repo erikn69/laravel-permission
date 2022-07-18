6496b8f6f702b46b39a6f32774c3d272a03cb7ad
erikn69 <erikn_69@hotmail.com>
2022-07-18 11:14:31 -0500

    Update Role.php

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -21,8 +21,6 @@ class Role extends Model implements RoleContract
 
     public function __construct(array $attributes = [])
     {
-        $attributes['guard_name'] = $attributes['guard_name'] ?? config('auth.defaults.guard');
-
         parent::__construct($attributes);
     }
 
